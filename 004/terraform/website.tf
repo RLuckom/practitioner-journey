918aaf5f5796852d500e1991ad99e7721cc3dfa1
Raphael Luckom <raphaelluckom@gmail.com>
2021-02-04 13:48:22 -0500

    pin module

--- 004/terraform/website.tf
+++ 004/terraform/website.tf
@@ -181,7 +181,7 @@ EOF
 }
 
 module website {
-  source = "github.com/RLuckom/terraform_modules//aws/serverless_site/tetrapod"
+  source = "github.com/RLuckom/terraform_modules//aws/serverless_site/tetrapod?ref=b1b400e6a57a9d501fd"
   maintainer = local.maintainer
   force_destroy = var.force_destroy
   nav_links = local.nav_links
