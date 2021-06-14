2341da67cf396d582629814cac2b273f911401de
Raphael Luckom <raphaelluckom@gmail.com>
2021-06-14 14:01:35 -0400

    update to ui plugins

--- 005/terraform/website.tf
+++ 005/terraform/website.tf
@@ -162,20 +162,20 @@ data "aws_caller_identity" "current" {}
 data "aws_region" "current" {}
 
 module "aws_sdk" {
-  source = "github.com/RLuckom/terraform_modules//snapshots/aws/layers/aws_sdk?ref=snapshots"
+  source = "github.com/RLuckom/terraform_modules//snapshots/aws/layers/aws_sdk?ref=89757f496551e05034807"
 }
 
 module "donut_days" {
-  source = "github.com/RLuckom/terraform_modules//snapshots/aws/layers/donut_days?ref=snapshots"
+  source = "github.com/RLuckom/terraform_modules//snapshots/aws/layers/donut_days?ref=89757f496551e05034807"
 }
 
 
 module "image_dependencies" {
-  source = "github.com/RLuckom/terraform_modules//snapshots/aws/layers/image_dependencies?ref=snapshots"
+  source = "github.com/RLuckom/terraform_modules//snapshots/aws/layers/image_dependencies?ref=89757f496551e05034807"
 }
 
 module "markdown_tools" {
-  source = "github.com/RLuckom/terraform_modules//snapshots/aws/layers/markdown_tools?ref=snapshots"
+  source = "github.com/RLuckom/terraform_modules//snapshots/aws/layers/markdown_tools?ref=89757f496551e05034807"
 }
 
 locals {
@@ -184,7 +184,7 @@ locals {
 }
 
 module human_attention_archive {
-  source = "github.com/RLuckom/terraform_modules//snapshots/aws/state/object_store/replicated_archive?ref=snapshots"
+  source = "github.com/RLuckom/terraform_modules//snapshots/aws/state/object_store/replicated_archive?ref=89757f496551e05034807"
   account_id = local.account_id
   region = local.region
   providers = {
@@ -211,23 +211,23 @@ module human_attention_archive {
 
 module admin_site_blog_plugin {
   source = "github.com/RLuckom/raphaelluckom.com//terraform/modules/plugins/blog"
-  admin_site_default_styles_path = module.admin_interface.default_styles_path
-  account_id = local.account_id
-  region = local.region
   name = "blog"
+  region = local.region
+  account_id = local.account_id
+  admin_site_resources = module.admin_interface.site_resources
+  coordinator_data = module.visibility_system.serverless_site_configs["alpha_blog"]
+  plugin_config = module.admin_interface.plugin_config["blog"]
   maintainer = local.maintainer
   nav_links = local.nav_links
   site_title = var.site_title
   logging_config = module.visibility_system.lambda_log_configs["alpha"]["human"].config
-  coordinator_data = module.visibility_system.serverless_site_configs["alpha_blog"]
-  plugin_config = module.admin_interface.plugin_config["blog"]
   image_layer = module.image_dependencies.layer_config
   donut_days_layer = module.donut_days.layer_config
   markdown_tools_layer = module.markdown_tools.layer_config
 }
 
 module admin_interface {
-  source = "github.com/RLuckom/terraform_modules//snapshots/aws/serverless_site/derestreet?ref=snapshots"
+  source = "github.com/RLuckom/terraform_modules//snapshots/aws/serverless_site/derestreet?ref=89757f496551e05034807"
   account_id = local.account_id
   region = local.region
   system_id = module.visibility_system.serverless_site_configs["alpha_admin"].system_id
@@ -236,14 +236,12 @@ module admin_interface {
   aws_sdk_layer = module.aws_sdk.layer_config
   plugin_static_configs = {
     blog = module.admin_site_blog_plugin.static_config
-    visibility = {
-      role_name_stem = "alphavis"
-      slug = "visibility"
-    }
+    visibility = module.admin_site_visibility_plugin.static_config
   }
   plugin_configs = {
     blog = {
-      additional_connect_sources = ["https://s3.amazonaws.com", "https://${module.admin_site_blog_plugin.blog_site_bucket_name}.s3.amazonaws.com", "https://${module.admin_interface.website_config.bucket_name}.s3.amazonaws.com"]
+      additional_connect_sources = module.admin_site_blog_plugin.additional_connect_sources_required
+      additional_style_sources = []
       policy_statements = []
       plugin_relative_lambda_origins = module.admin_site_blog_plugin.plugin_relative_lambda_origins
       plugin_relative_bucket_upload_permissions_needed = module.admin_site_blog_plugin.plugin_relative_bucket_upload_permissions_needed
@@ -254,8 +252,9 @@ module admin_interface {
       file_configs = module.admin_site_blog_plugin.files
     }
     visibility = {
+      additional_connect_sources = []
+      additional_style_sources = []
       policy_statements = []
-      additional_connect_sources = ["https://athena.us-east-1.amazonaws.com", "https://s3.amazonaws.com", "https://${module.admin_interface.website_config.bucket_name}.s3.amazonaws.com"] 
       plugin_relative_lambda_origins = []
       storage_path_lambda_notifications = []
       upload_path_lambda_notifications = []
@@ -273,7 +272,7 @@ module admin_interface {
 }
 
 module visibility_system {
-  source = "github.com/RLuckom/terraform_modules//snapshots/aws/visibility/aurochs?ref=snapshots"
+  source = "github.com/RLuckom/terraform_modules//snapshots/aws/visibility/aurochs?ref=89757f496551e05034807"
   account_id = local.account_id
   region = local.region
   cloudfront_delivery_bucket = "${local.bucket_prefix}-cloudfront-delivery"
@@ -317,5 +316,9 @@ module visibility_system {
 
 module admin_site_visibility_plugin {
   source = "github.com/RLuckom/raphaelluckom.com//terraform/modules/plugins/visibility"
-  default_styles_path = module.admin_interface.default_styles_path
+  name = "visibility"
+  account_id = local.account_id
+  region = local.region
+  admin_site_resources = module.admin_interface.site_resources
+  plugin_config = module.admin_interface.plugin_config["visibility"]
 }
